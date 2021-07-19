3e9f9fd1b50d1facc40fa6cdb1e38cf0838b50ad
glucicoools <gloriaegedi@gmail.com>
2021-07-19 04:36:58 -0700

    Retrofitapp

--- app/src/main/java/com/gloria/retrofitapp/UI/MainViewModel.kt
+++ app/src/main/java/com/gloria/retrofitapp/UI/MainViewModel.kt
@@ -5,22 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.gloria.retrofitapp.api.RetrofiProvider
 import com.gloria.retrofitapp.model.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainViewModel: ViewModel() {
     val postLiveData = MutableLiveData<List<User>>()
 
             fun getUsers(){
-                RetrofiProvider.placeHolderAPI.getUsers().enquene(object : Callback<List<User>>){
-                    override fun onResponse(call: Call<List<User>>, response: Response<List<User>>){
-                        response.body()?.let {
-                            usersLiveData.postValue(it)
-                        }
-                    }
-                    override fun onFailure(call: Call<List<User>>, t: Throwable){
+                coroutineScope (Dispatchers.IO).launch{
+                    usersLivesData.postValue(RetrofitProvider.placeHolderAPI.getUsers())
+                }
 
                     }
                 }
-            }
-}
+
